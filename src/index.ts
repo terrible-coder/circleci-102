4725626386e30be87d84f6d91c6249ccd60ca773
Ayanava De <ayanavade01@gmail.com>
2020-05-07 20:28:57 +0530

    Moving files in different folder

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+export function add(a: number, b: number) {
+	return a + b;
+}
+
+/*
+This is a completely meaningless change. Nothing should break. CircleCI is not
+supposed to deploy anything to npm either.
+*/
+
+/*
+This a second completely meaningless change. Nothing should break. CircleCI is
+supposed to deploy the package.
+*/
+
+/*
+Third attempt.
+*/
+
+/*
+Fourth attempt.
+*/
